b0d0e344183c25b0c8b734d9912e8f82febfbd62
GangneuxLuc <l.gangneux@bdx.ecolescreatives.com>
2026-03-31 10:59:02 +0200

    Collision Fix

--- Rookie_Alchemist/Assets/_Scripts/Interactor.cs
+++ Rookie_Alchemist/Assets/_Scripts/Interactor.cs
@@ -20,12 +20,49 @@ public class Interactor : MonoBehaviour
 
     private void Update() // gros caca
     {
+<<<<<<< Updated upstream
         if (Input.GetKeyDown(KeyCode.E))
         {
             Ray r = new Ray(interactorSource.position, interactorSource.forward * interactorRange);
             if (Physics.Raycast(r, out RaycastHit hitInfo, interactorRange, layerMask))
                 Debug.Log(hitInfo.collider.gameObject.name);
                 Debug.DrawRay(interactorSource.position, interactorSource.forward * interactorRange, Color.red, 1f);
+=======
+       /* if (coroutine == null && Input.GetKeyDown(KeyCode.E))
+        {
+            coroutine = StartCoroutine(RayCast());
+            coroutine = null;
+        } */
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Ray r = new Ray(interactorSource.position, interactorSource.forward * interactorRange);
+            Debug.DrawRay(interactorSource.position, interactorSource.forward * interactorRange, Color.red, 1f);
+            if (Physics.Raycast(r, out RaycastHit hitInfo, interactorRange, layerMask))
+         
+            {
+                if (hitInfo.collider.gameObject.TryGetComponent(out IInteractable interactObj))
+                {
+                    interactObj.Interact();
+                }
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+      
+        }
+    }
+    IEnumerator RayCast()
+    {
+      
+        Ray r = new Ray(interactorSource.position, interactorSource.forward * interactorRange);
+        if (Physics.Raycast(r, out RaycastHit hitInfo, interactorRange, layerMask))
+         Debug.Log(hitInfo.collider.gameObject.name);
+        Debug.DrawRay(interactorSource.position, interactorSource.forward * interactorRange, Color.red, 1f);
+        {
+            if (hitInfo.collider.gameObject.TryGetComponent(out IInteractable interactObj))
+>>>>>>> Stashed changes
             {
                 if (hitInfo.collider.gameObject.TryGetComponent(out IInteractable interactObj))
                 {
--- Rookie_Alchemist/Assets/_Scripts/ObjectsToGather.cs
+++ Rookie_Alchemist/Assets/_Scripts/ObjectsToGather.cs
@@ -9,9 +9,37 @@ public class ObjectsToGather : MonoBehaviour, IInteractable
     public Transform[] randomEmplacements;
     public void Interact()
     {
+<<<<<<< Updated upstream
         Debug.Log(Random.Range(0, 100));
         transform.position = randomEmplacements[Random.Range(0, randomEmplacements.Length)].position;
+=======
+        // Fix la foce pour briser le joint et faire tomber l'objet
+        // Faire une UI qui signale une interaction possible lorsqu'on est assez proche de l'objet
 
+        foreach (Transform emplacement in randomEmplacements)
+        {
+            if (Vector3.Distance(transform.position, emplacement.position) < 0.5f)
+            {
+                transform.position = emplacement.position + Vector3.up * height;
+                gameObject.layer = LayerMask.NameToLayer("Plateau");
+                return;
+            }
+        }
+    }
+>>>>>>> Stashed changes
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            foreach (Random.Range(0, randomEmplacements.Length) in randomEmplacements)
+            /* transform.position = randomEmplacements[Random.Range(0, randomEmplacements.Length)].position + Vector3.up * height;
+             joint = gameObject.AddComponent<FixedJoint>();
+             joint.connectedBody = rbPlateau;
+             joint.breakForce = float.PositiveInfinity;
+             joint.breakTorque = 100f;
+             //joint.enableCollision = true; */
+        }
     }
 }
 
